8dfa739a7b2c86b24098c09fcb62e3645f38e737
Muhammad Kamran Khalid <mkamran.khalid.dev@gmail.com>
2026-05-31 00:22:36 +0500

    Added Obstacles and Boundary Wall to Game

--- Obstacle Odyssey/Assets/Scripts/PlayerController.cs
+++ Obstacle Odyssey/Assets/Scripts/PlayerController.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float xValue = 0.01f;
-    float yValue = 0.01f;
-    float zValue = 0.01f;
+    [SerializeField] float moveSpeed = 5f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
         transform.Translate(xValue, yValue, zValue);
     }
 }
